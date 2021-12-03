e53a3e8c28d58a816d078a58102859b0c35a7a34
Arsinoe-Z <93827081+Arsinoe-Z@users.noreply.github.com>
2021-12-03 17:28:15 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main(void)
+{
+	// About the program
+	puts("Siling Probinsya Order Taker");
+	puts("Copyright (c) 2021 Arsinoe-Z et alia. All rights reserved.\n");
+	
+	// Prompt to press any key
+    printf("Press any key to continue... ");
+    getch();
+	
+	// Clear screen and display the next user interface
+    system("cls");
+	puts("*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*");
+	puts(".          Welcome to Siling Probinsya          .");
+	puts("*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*");
+	
+	return 0;
+}

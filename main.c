255d7fbcb286183972f6f29df1f559ba51430632
Arsinoe-Z <93827081+Arsinoe-Z@users.noreply.github.com>
2021-12-03 17:36:30 +0800

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(void)
 	puts("Siling Probinsya Order Taker");
 	puts("Copyright (c) 2021 Arsinoe-Z et alia. All rights reserved.\n");
 	
-	// Prompt to press any key
+	// Prompt user to press any key
     printf("Press any key to continue... ");
     getch();
 	

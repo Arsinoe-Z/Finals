18a96602bae841aadbaf015f22e3a3673b561430
Arsinoe-Z <93827081+Arsinoe-Z@users.noreply.github.com>
2021-12-03 17:38:58 +0800

    Update main.c

--- main.c
+++ main.c
@@ -9,11 +9,11 @@ int main(void)
 	puts("Copyright (c) 2021 Arsinoe-Z et alia. All rights reserved.\n");
 	
 	// Prompt user to press any key to continue
-    printf("Press any key to continue... ");
-    getch();
+	printf("Press any key to continue... ");
+	getch();
 	
 	// Clear screen and display the next user interface
-    system("cls");
+	system("cls");
 	puts("*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*");
 	puts(".          Welcome to Siling Probinsya          .");
 	puts("*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*");
